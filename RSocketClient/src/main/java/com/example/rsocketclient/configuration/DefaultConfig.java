33665ac9eff75b14387816957a10e36fe4e6bf6a
Pol-cyber <yarema.ketta2002@gmail.com>
2024-06-20 10:52:39 +0300

    Create RSocket models: request-stream, fire-and-forget, channel

--- RSocketClient/src/main/java/com/example/rsocketclient/configuration/DefaultConfig.java
+++ RSocketClient/src/main/java/com/example/rsocketclient/configuration/DefaultConfig.java
@@ -5,6 +5,9 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.rsocket.RSocketRequester;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @Configuration
 public class DefaultConfig {
@@ -14,10 +17,31 @@ public class DefaultConfig {
         return args -> {
              RSocketRequester requester = builder.tcp("localhost",7000);
 
-             requester.route("/")
-                     .data("Hello")
-                     .retrieveMono(String.class)
-                     .subscribe(s -> System.out.println("Result is "+s));
+             // Request-response
+//             requester.route("/requestResponse")
+//                     .data("Hello")
+//                     .retrieveMono(String.class)
+//                     .subscribe(s -> System.out.println("Result is "+s));
+
+            // request-stream
+//            requester.route("/requestStream")
+//                     .data("Hello")
+//                     .retrieveFlux(Character.class)
+//                     .subscribe(s -> System.out.println("Character is  "+s));
+
+            // fire-and-forget
+//            requester.route("/fire-and-forget")
+//                    .data("TestFire")
+//                    .send() // method 'send' because we don't expect result
+//                    .subscribe();
+
+            // channel
+            Flux<String> flux = Flux.just("Test","Channel","Model").delayElements(Duration.ofSeconds(1));
+            requester.route("/channel")
+                    .data(flux)
+                    .retrieveFlux(String.class) // method 'send' because we don't expect result
+                    .subscribe(s -> System.out.println(s +" Result"));
+
         };
     }
 }
--- TestingSpringWebFlux/src/main/java/com/example/testingspringwebflux/controller/RSocketController.java
+++ TestingSpringWebFlux/src/main/java/com/example/testingspringwebflux/controller/RSocketController.java
@@ -4,16 +4,45 @@ package com.example.testingspringwebflux.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @Controller
 @Slf4j
 public class RSocketController {
 
-    @MessageMapping("/")
-    public Mono<String> stringMono(Mono<String> stringMono){
+    @MessageMapping("/requestResponse")
+    public Mono<String> rResponse(Mono<String> stringMono){
         return stringMono.doOnNext(s -> {
             log.info("Message Receive");
         }).map(mapper -> "New String return");
+    }
+
+
+    @MessageMapping("/requestStream")
+    public Flux<Character> rStream(Mono<String> stringMono){
+        return stringMono.flux().flatMap(str -> Flux.fromArray(toCharacterArray(str)))
+                .delayElements(Duration.ofSeconds(1));
+    }
+
+    @MessageMapping("/fire-and-forget")
+    public Mono<Void> fForget(Mono<String> s){
+        return s.doOnNext(s1 -> log.info(s1)).thenEmpty(Mono.empty());
+    }
+
+    @MessageMapping("/channel")
+    public Flux<String> fForget(Flux<String> s){
+        return s.doOnNext(s1 -> log.info(s1 +" Server"));
+    }
+
+    private Character[] toCharacterArray(String str) {
+        char[] charArray = str.toCharArray();
+        Character[] characterArray = new Character[charArray.length];
+        for (int i = 0; i < charArray.length; i++) {
+            characterArray[i] = charArray[i];
+        }
+        return characterArray;
     }
 }
